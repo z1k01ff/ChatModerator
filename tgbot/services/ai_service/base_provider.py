a767e176d326b6c7920fb0ddd5a6ab4492674436
Latand <latand666@gmail.com>
2024-05-14 20:38:59 +0000

    Refactor AIMediaBase and AIProviderBase to use abstract methods

--- tgbot/services/ai_service/base_provider.py
+++ tgbot/services/ai_service/base_provider.py
@@ -1,23 +1,28 @@
 from io import BytesIO
 from typing import AsyncGenerator, List, Optional
 
+from abc import ABC, abstractmethod
 
-class AIMediaBase:
+
+class AIMediaBase(ABC):
     def __init__(self, photo: BytesIO, mime_type: str = "image/jpeg"):
         self.photo = photo
         self.mime_type = mime_type
 
+    @abstractmethod
     def prepare_photo(self) -> str:
         raise NotImplementedError("This method should be overridden by subclasses")
 
+    @abstractmethod
     def render_content(self, text: Optional[str] = None) -> list:
         raise NotImplementedError("This method should be overridden by subclasses")
 
 
-class AIProviderBase:
+class AIProviderBase(ABC):
     def __init__(self, media_class: AIMediaBase):
         self.media_class = media_class
 
+    @abstractmethod
     async def generate_response(
         self,
         messages: List[dict],
